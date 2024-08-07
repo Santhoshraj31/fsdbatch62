0e68aa223fa2808e5471b0218660639ef6e9044b
Santhoshraj31 <santhoshrajan1995@gmail.com>
2024-08-07 11:46:20 +0530

    first commit

---
+++ day 3/script.js
@@ -0,0 +1,22 @@
+// function arithmetic(a,b) {
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+// }
+// arithmetic(23,23)
+
+
+function arithmetic(a,b) {
+    return [a+b,a-b,a*b,a%b]
+    
+}
+console.log(arithmetic(23,7));
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let result = []
+// for (let i=0,i<=arr.lenghth,i++)
+// {
+//     if(i%2!==0)
+//         result.push(i);
+// }
+// console.log(result);
